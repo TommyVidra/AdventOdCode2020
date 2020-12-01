637b4a6089a2d07e88f3c455cd15f4365726c066
VidraTo <tomislav.vidovic@fer.hr>
2020-12-01 17:52:46 +0100

    Nadodan prvi dan

---
+++ AdventOfCode2020/AdventOfCode2020/Day1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace AdventOfCode2020
+{
+    class Day1
+    {
+
+        private int sum = 2020;
+        private int result = 0;
+        private static List<int> inputs;
+        public Day1(List<int> list)
+        {
+            inputs = list;
+            inputs.Sort();
+        }
+
+        public int firstTask()
+        {
+            foreach(int i in inputs)
+            {
+                int second = sum - i;
+                if (inputs.Contains(second))
+                    result = second * i;
+            }
+            return result;
+        }
+
+        public int secondTask()
+        {
+            foreach (int i in inputs)
+            {
+                int second = sum - i;
+                if (!(inputs.Contains(second)))
+                {
+                    for(int j = inputs.IndexOf(i); j < inputs.Count; ++j)
+                    {
+                        if (inputs.Contains(second - inputs[j]))
+                            result = i * (second - inputs[j]) * inputs[j];
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ AdventOfCode2020/AdventOfCode2020/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AdventOfCode2020
+{
+    class Program
+    {
+        private static string directory = "E:/Projects/AoC/AdventOfCode2020/AdventOfCode2020"; 
+        private static List<int> returnInput()
+        {
+            var fileName = directory + "/inputs.txt";
+            String[] input = File.ReadAllLines(fileName);
+
+            List<int> list = new List<int>();
+            foreach (String s in input)
+                list.Add(Convert.ToInt32(s));
+
+            return list;
+        }
+
+        static void Main(string[] args)
+        {
+
+            Day1 task = new Day1(returnInput());
+            Console.WriteLine(task.firstTask());
+            Console.WriteLine(task.secondTask());
+        }
+    }
+}
